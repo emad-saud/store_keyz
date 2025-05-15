f06fce52495b3bbb121a1f87d8afb0837853b685
Emad Saud <emad.k.saud@gmail.com>
2025-05-15 18:52:09 +0200

    created the order routes

---
+++ src/routes/orderRoutes.ts
@@ -0,0 +1,25 @@
+import { Router } from 'express';
+
+import {
+  createOrder,
+  deleteOrder,
+  getAllOrders,
+  getMyOrders,
+  getUserOrders,
+} from '../controllers/orderController';
+import { protect, restrictTo } from '../controllers/authController';
+import Role from '../enums/roles';
+
+const router = Router();
+
+router.use(protect);
+
+router.post('/', createOrder);
+router.get('/my-orders', getMyOrders);
+
+// Super User routes
+router.use(restrictTo([Role.SuperUser]));
+router.get('/get-all-orders', getAllOrders);
+router.get('/:userId', getUserOrders);
+
+export default router;
