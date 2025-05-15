d47371301023a3ecfacefd2a7cac82dcaee15a86
Emad Saud <emad.k.saud@gmail.com>
2025-05-15 18:52:21 +0200

    added more category routes

--- src/routes/categoryRoutes.ts
+++ src/routes/categoryRoutes.ts
@@ -1,7 +1,6 @@
 import { Router } from 'express';
 
 import { protect, restrictTo } from '../controllers/authController';
-
 import {
   createCategory,
   deleteCategory,
@@ -9,10 +8,20 @@ import {
   getCategory,
   updateCategory,
 } from '../controllers/categoryController';
+import {
+  uploadCategoryImage,
+  resizeCategoryImage,
+} from '../controllers/imageUploadController';
+import Role from '../enums/roles';
 
 const router = Router();
 
-router.route('/').get(getAllCategories).post(createCategory);
+router.use(protect, restrictTo([Role.Admin, Role.SuperUser]));
+
+router
+  .route('/')
+  .get(getAllCategories)
+  .post(uploadCategoryImage, resizeCategoryImage, createCategory);
 
 router
   .route(':categoryId')
