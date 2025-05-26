a7a21a1a1e4876910ecf589272cb1a276844a5f9
Emad Saud <emad.k.saud@gmail.com>
2025-05-27 00:38:28 +0200

    added type casting for the Item model class

--- src/models/itemModel.ts
+++ src/models/itemModel.ts
@@ -1,4 +1,13 @@
-import { Model, DataTypes, Sequelize, Optional } from 'sequelize';
+import {
+  Model,
+  DataTypes,
+  Sequelize,
+  Optional,
+  Includeable,
+  FindOptions,
+} from 'sequelize';
+
+import { Product } from '../models';
 
 interface ItemAttributes {
   id: string;
@@ -9,7 +18,13 @@ interface ItemAttributes {
 }
 interface ItemCreationAttributes extends Optional<ItemAttributes, 'id'> {}
 export default (db: Sequelize) => {
-  class Item extends Model<ItemAttributes, ItemCreationAttributes> {}
+  class Item extends Model<ItemAttributes, ItemCreationAttributes> {
+    public id!: string;
+    public productId!: string;
+    public valid!: boolean;
+    public content!: string;
+    public soldAt!: Date;
+  }
 
   Item.init(
     {
@@ -37,8 +52,37 @@ export default (db: Sequelize) => {
     {
       sequelize: db,
       modelName: 'Item',
+      indexes: [
+        {
+          unique: true,
+          fields: ['productId', 'content'],
+        },
+      ],
     }
   );
+
+  Item.addHook('beforeFind', (options: FindOptions<ItemAttributes>) => {
+    if (!options.include) {
+      options.include = [];
+    } else if (!Array.isArray(options.include)) {
+      options.include = [options.include];
+    }
+
+    const includeArray = options.include as Includeable[];
+    const alreadyIncluded = options.include.some((include) => {
+      if (typeof include === 'object' && 'model' in include) {
+        return include.model === Product;
+      }
+      return false;
+    });
+
+    if (!alreadyIncluded) {
+      includeArray.push({
+        model: Product,
+      });
+    }
+    options.include = includeArray;
+  });
 
   return Item;
 };
